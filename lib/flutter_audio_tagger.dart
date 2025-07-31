f10f04af848b9d4a57a4a6a8c78bb01ad46875a4
vrtkarim <elyaagoubi.karim03@gmail.com>
2025-07-31 20:03:01 +0100

    small improvements

--- lib/flutter_audio_tagger.dart
+++ lib/flutter_audio_tagger.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
 import 'dart:typed_data';
-
 import 'package:flutter/services.dart';
 import 'package:flutter_audio_tagger/tag.dart';
-
 import 'flutter_audio_tagger_platform_interface.dart';
 
 class FlutterAudioTagger {
@@ -14,6 +11,7 @@ class FlutterAudioTagger {
   }
 
   Future<Uint8List?> getArtWork(String path) async {
+    // this method is used to get the cover of the song
     try {
       final result = await platform.invokeMethod<Uint8List>('getArtWork', path);
       return result;
@@ -23,6 +21,7 @@ class FlutterAudioTagger {
   }
 
   Future<Map<dynamic, dynamic>?> getTags(String path) async {
+    //this methos is used to get the metadata of the song
     try {
       final result = await platform.invokeMethod<Map>('getTags', path);
       Map<dynamic, dynamic>? tags = result;
@@ -33,6 +32,7 @@ class FlutterAudioTagger {
   }
 
   Future<Tag?> getAllTags(String path) async {
+    // this method is used to get the tags also the cover image of the song
     try {
       Tag tag = Tag();
       late Map<dynamic, dynamic>? data;
@@ -47,6 +47,8 @@ class FlutterAudioTagger {
   }
 
   Future<void> editTags(Tag tag, String path) async {
+    // this method is used to edit tags of the song
+
     try {
       Map<String, String?> tags = Tag.createMapWithPath(tag, path);
       await platform.invokeMethod<String>('setTags', tags);
@@ -61,14 +63,14 @@ class FlutterAudioTagger {
   }
 
   Future<void> setArtWork(Uint8List? imageData, String path) async {
+    //this method is used to edit the image cover of the song, make sure that the imageData is not null
     if (imageData == null) {
       return;
     }
 
     try {
       Map<String, dynamic> artwork = {'artwork': imageData, 'filePath': path};
-      final result = await platform.invokeMethod<String>("setArtWork", artwork);
-      
+      await platform.invokeMethod<String>("setArtWork", artwork);
     } catch (e) {
       rethrow;
     }
