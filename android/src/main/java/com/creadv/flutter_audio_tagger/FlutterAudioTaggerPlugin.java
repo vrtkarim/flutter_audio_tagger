7a84c9b07f986dc9030912002eb0b7305f1cb347
vrtkarim <elyaagoubi.karim03@gmail.com>
2025-08-05 21:30:34 +0100

    file handlinig improvements

--- android/src/main/java/com/creadv/flutter_audio_tagger/FlutterAudioTaggerPlugin.java
+++ android/src/main/java/com/creadv/flutter_audio_tagger/FlutterAudioTaggerPlugin.java
@@ -14,6 +14,7 @@ import android.util.Log;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -197,6 +198,13 @@ public class FlutterAudioTaggerPlugin implements FlutterPlugin, MethodCallHandle
                 audioFile.setTag(tag);
                 AudioFileIO.write(audioFile);
                 File musicfile = new File(downloadsDir, addEditedSuffix(filePath));
+                if (musicfile.exists()) {
+                    try {
+                        Files.delete(Paths.get(musicfile.getPath()));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
                 try (FileOutputStream fos = new FileOutputStream(musicfile)){
                     byte[] music_data = Files.readAllBytes(Paths.get(audioFile.getFile().getPath()));
                     fos.write(music_data);
@@ -242,6 +250,13 @@ public class FlutterAudioTaggerPlugin implements FlutterPlugin, MethodCallHandle
                     }
                     AudioFileIO.write(f);
                     File musicfile = new File(downloadsDir, addEditedSuffix(filePath));
+                    if (musicfile.exists()) {
+                        try {
+                            Files.delete(Paths.get(musicfile.getPath()));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                     try(FileOutputStream fos = new FileOutputStream(musicfile)) {
 
                         byte[] music_data = Files.readAllBytes(Paths.get(f.getFile().getPath()));
